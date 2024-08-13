7811aac09a09f81551891e2da90afe82ac7db328
Andrea Rufo <a.rufo@axio.studio>
2024-08-13 16:19:59 +0200

    Implementazione

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <ItalyMap />
+    <div class="container">
+        
+        <ItalyMap @select="checkRegione" :active="activeRegion" />
+
+    </div>
 </template>
 
 <script>
 import ItalyMap from './components/ItalyMap.vue'
 
-export default{
-  name: 'App',
-  
-  components: {
-    ItalyMap
-  }
+export default {
+    name: 'App',
+
+    data(){
+        return {
+            activeRegion: null
+        }
+    },
+
+    components: {
+        ItalyMap
+    },
+
+    methods: {
+        checkRegione(regione) {
+            console.log('Click ok', regione);
+            this.activeRegion = regione
+        }
+    }
 }
 </script>
+
+<style lang="scss">
+    .container {
+        width: 960px;
+        margin: auto;
+        height: 100vh;
+    }
+</style>
